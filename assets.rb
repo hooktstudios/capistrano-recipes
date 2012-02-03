2f9430e4abac3eeb1d637bdeb6cfea10fa0b790b
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-02-03 11:16:02 -0500

    Assets (less/sass) recipe

---
+++ assets.rb
@@ -0,0 +1,14 @@
+set :lessc_path, "~/node_modules/less/bin/lessc"
+set :sass_path, "sass"
+
+namespace :assets do
+  desc "Compile LESS files"
+  task :less_compile, :roles => :app do
+    run "find #{release_path} -name \"*.less\" | xargs -I {} #{lessc_path} -x {} {}.css"
+  end
+
+  desc "Compile SASS files"
+  task :sass_compile, :roles => :app do
+    run "find #{release_path} -name \"*.scss\" -o -name \"*.sass\" | xargs -I {} #{sass_path} -x {} {}.css"
+  end
+end

ddce1540976ef881ba49fcca82b2151536045b9d
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-02-03 15:59:19 -0500

    Fix WordPress recipe task name + overwrite default finalize too

--- wordpress.rb
+++ wordpress.rb
@@ -18,11 +18,16 @@ namespace :deploy do
   task :stop, :roles => :app, :except => { :no_release => true } do
     # do nothing
   end
+
+  desc "This is here to overide the original task"
+  task :finalize_update, :roles => :app, :except => { :no_release => true } do
+    # do nothing
+  end
 end
 
 namespace :wordpress do
   desc "Link all the config files to the shared directory"
-  task :link_configs do
+  task :update_code do
     run "ln -s #{File.join(shared_path, 'wp-config.php')} #{latest_release}"
 
     shared_children.each do |shared_child|

dd7f04aa86020c292e0f13dbbb4a9cf5ec0981ab
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-02-03 11:58:35 -0500

    Removed hard tabs

--- php-fpm.rb
+++ php-fpm.rb
@@ -1,6 +1,6 @@
 namespace :php_fpm do
-	desc "Reload PHP5-FPM (requires sudo access to /usr/sbin/service php5-fpm reload)"
-	task :reload, :roles => :app do
-		run "sudo /usr/sbin/service php5-fpm reload"
-	end
+  desc "Reload PHP5-FPM (requires sudo access to /usr/sbin/service php5-fpm reload)"
+  task :reload, :roles => :app do
+    run "sudo /usr/sbin/service php5-fpm reload"
+  end
 end

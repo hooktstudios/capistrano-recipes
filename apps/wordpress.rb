aea8e2f71dd0234d1b822f9ede192765d154c243
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-03-06 23:28:46 -0500

    Fix Wordpress recipe path variable

--- apps/wordpress.rb
+++ apps/wordpress.rb
@@ -28,10 +28,10 @@ end
 namespace :wordpress do
   desc "Link all the config files to the shared directory"
   task :update_code do
-    run "ln -s #{File.join(shared_path, 'wp-config.php')} #{latest_release}"
+    run "ln -s #{File.join(shared_path, 'wp-config.php')} #{release_path}"
 
     shared_children.each do |shared_child|
-      run "if [ ! -h #{File.join(current_path, shared_child)} ]; then rm -rf #{File.join(latest_release, shared_child)} && ln -s #{File.join(shared_path, shared_child)} #{File.join(latest_release, shared_child)}; fi"
+      run "if [ ! -h #{File.join(current_path, shared_child)} ]; then rm -rf #{File.join(release_path, shared_child)} && ln -s #{File.join(shared_path, shared_child)} #{File.join(release_path, shared_child)}; fi"
     end
   end
 end

d85660e5d600b92b5b302db887341216f1c83308
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-06-03 13:06:54 -0300

    Add Webistrano licence

--- apps/mod_rails.rb
+++ apps/mod_rails.rb
@@ -1,5 +1,22 @@
-# From Webistrano template
+#
+# From Webistrano template (removed webistrano specifics)
+#
+# Copyright (c) 2007-2008 Jonathan Weiss <jw@innerewut.de>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 # @see https://github.com/peritor/webistrano/blob/master/lib/webistrano/template/mod_rails.rb
+#
 
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do

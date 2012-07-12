e060478f80ff8d5e50099033c9e9d687c5096b92
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-07-12 15:58:48 -0400

    Use remote repository for assets precompiling check

--- apps/rails_turbo_assets.rb
+++ apps/rails_turbo_assets.rb
@@ -1,5 +1,13 @@
+#
+# There are a few requirement for theses tasks to work as intentended :
+#
+# - You must set :deploy_via, :remote_cache
+# - The cached copy was synced previously during deployment 
+#   (otherwise real_revision might not be yet fetch on the cached copy)
+
 set(:skip_assets_precompile) do
-  run_locally("#{source.local.log(current_revision, revision)} --oneline vendor/assets/ app/assets/ | wc -l").to_i == 0
+  cached_copy = File.join(shared_path, fetch(:repository_cache, "cached-copy"))
+  run("cd #{cached_copy} && #{source.log(current_revision, real_revision)} --oneline vendor/assets/ app/assets/ | wc -l").to_i == 0
 end
 
 namespace :deploy do
